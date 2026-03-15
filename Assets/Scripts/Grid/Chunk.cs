b1b0f9c75bb917cebeef8b41037fb6800c10d756
GOPI5757 <65418518+GOPI5757@users.noreply.github.com>
2026-03-15 20:08:23 +0530

    Chunk

--- Assets/Scripts/Grid/Chunk.cs
+++ Assets/Scripts/Grid/Chunk.cs
@@ -1,18 +1,214 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace DiggingGame.Grid
 {
     [System.Serializable]
     public struct BlockData
     {
-        public Vector2Int coord;
+        public Vector3Int coord;
         public BlockType blockType;
         public int strength;
         public Vector3 worldPos;
+        public Vector3 localPos;
+    
+        public BlockData(Vector3Int coord, BlockType blockType, int strength, Vector3 worldPos, Vector3 localPos)
+        {
+            this.coord = coord;
+            this.blockType = blockType;
+            this.strength = strength;
+            this.worldPos = worldPos;
+            this.localPos = localPos;
+        }
     }
 
+    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
     public class Chunk : MonoBehaviour
     {
-        [SerializeField] private BlockData[] blockDatas;
+        [SerializeField] private Vector3Int chunkSize;
+
+        [SerializeField] private List<Vector3> vertices = new List<Vector3>();
+        [SerializeField] private List<int> triangles = new List<int>();
+
+        [SerializeField] private BlockData[,,] block_Datas;
+
+        private Vector3[,] vertexData;
+        private int[] triangleData;
+        private Vector3Int[] boundCheckVector;
+
+        private Mesh mesh;
+
+        private void Start()
+        {
+            StoreInitialValues();
+            PrepareBlockData();
+            CreateShape();
+            UpdateMesh();
+        }
+
+        private void StoreInitialValues()
+        {
+            mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+            block_Datas = new BlockData[chunkSize.x, chunkSize.y, chunkSize.z];
+
+            vertexData = new Vector3[,]
+            {
+                {
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, 1, 0),
+                    new Vector3(1, 1, 0), // Front
+                    new Vector3(1, 0, 0),
+                },
+                {
+                    new Vector3(1, 0, 0),
+                    new Vector3(1, 1, 0),
+                    new Vector3(1, 1, 1), // Right
+                    new Vector3(1, 0, 1),
+                },
+                {
+                    new Vector3(1, 0, 1),
+                    new Vector3(1, 1, 1),
+                    new Vector3(0, 1, 1), // Back
+                    new Vector3(0, 0, 1),
+                },
+                {
+                    new Vector3(0, 0, 1),
+                    new Vector3(0, 1, 1),
+                    new Vector3(0, 1, 0), // Left
+                    new Vector3(0, 0, 0),
+                },
+                {
+                    new Vector3(0, 1, 0),
+                    new Vector3(0, 1, 1),
+                    new Vector3(1, 1, 1), // Top
+                    new Vector3(1, 1, 0),
+                },
+                {
+                    new Vector3(0, 0, 0),
+                    new Vector3(1, 0, 0),
+                    new Vector3(1, 0, 1), // Bottom
+                    new Vector3(0, 0, 1)
+                }
+            };
+
+            triangleData = new int[] { 0, 1, 2, 0, 2, 3 };
+
+            boundCheckVector = new Vector3Int[]
+            {
+                new Vector3Int(0, 0, -1),
+                new Vector3Int(1, 0, 0),
+                new Vector3Int(0, 0, 1),
+                new Vector3Int(-1, 0, 0),
+                new Vector3Int(0, 1, 0),
+                new Vector3Int(0, -1 ,0)
+            };
+        }
+
+        private void PrepareBlockData()
+        {
+            for (int i = 0; i < chunkSize.x; i++)
+            {
+                for (int j = 0; j < chunkSize.y; j++)
+                {
+                    for (int k = 0; k < chunkSize.z; k++)
+                    {
+                        BlockData item = new BlockData(
+                            new Vector3Int(i, j, k),
+                            BlockType.Sand,
+                            5,
+                            transform.position + new Vector3(i, j, k),
+                            new Vector3(i, j, k)
+                        );
+
+                        block_Datas[i, j, k] = item;
+                    }
+                }
+            }
+        }
+    
+        private void CreateShape()
+        {
+            for(int i = 0; i < chunkSize.x; i++)
+            {
+                for(int j = 0; j < chunkSize.y; j++)
+                {
+                    for(int k = 0; k < chunkSize.z; k++)
+                    {
+                        for(int l = 0; l < 6; l++)
+                        {
+                            Vector3Int neighbour = block_Datas[i, j, k].coord + boundCheckVector[l];
+                            bool flag = true;
+                            if (CheckBounds(neighbour))
+                            {
+                                flag = IsCoordFree(neighbour);
+                            }
+
+                            if(flag)
+                            {
+                                for(int p = 0; p < 4; p++)
+                                {
+                                    vertices.Add(block_Datas[i, j, k].localPos + vertexData[l, p]);
+                                }
+                            
+                                for(int m = 0; m < 6; m++)
+                                {
+                                    triangles.Add((vertices.Count - 4) + triangleData[m]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool CheckBounds(Vector3Int coord)
+        {
+            return (
+                CheckCoordBound(coord.x, chunkSize.x) &&
+                CheckCoordBound(coord.y, chunkSize.y) &&
+                CheckCoordBound(coord.z, chunkSize.z)
+            );
+        }
+
+        private bool CheckCoordBound(int val, int size)
+        {
+            return (val >= 0 && val < size);
+        }
+
+        private bool IsCoordFree(Vector3Int coord)
+        {
+            return block_Datas[coord.x, coord.y, coord.z].blockType == BlockType.Air;   
+        }
+
+        private void UpdateMesh()
+        {
+            if (mesh == null) return;
+
+            mesh.Clear();
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals();
+
+            GetComponent<Renderer>().material.color = Color.sandyBrown;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!Application.isPlaying) return;
+            for(int i = 0; i < chunkSize.x; i++)
+            {
+                for(int j = 0; j < chunkSize.y; j++)
+                {
+                    for(int k = 0; k < chunkSize.z; k++)
+                    {
+                        Gizmos.DrawWireCube(block_Datas[i, j, k].worldPos + (Vector3.one / 2), Vector3.one);
+                    }
+                }
+            }
+        }
+
+        public void SetChunk(Vector3Int val) { chunkSize = val; }
     }
 }
--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -3,98 +3,27 @@ using System.Collections.Generic;
 
 namespace DiggingGame.Grid
 {
-    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
     public class GridManager : MonoBehaviour
     {
         [SerializeField] private float defaultBlockSize;
         [SerializeField] private GameObject chunkPrefab;
 
-        private Vector3[] vertices;
-        private int[] triangles;
-        Mesh mesh;
+        [SerializeField] private Vector3Int chunkSize;
 
+        private void Awake()
+        {
+            
+        }
 
         void Start()
         {
-            mesh = new Mesh();
-            GetComponent<MeshFilter>().mesh = mesh;
-
-            CreateShape();
-            UpdateMesh();
-        }
-
-        private void CreateShape()
-        {
-            vertices = new Vector3[]
+            Vector3 pos = Vector3.zero - new Vector3(chunkSize.x / 2, chunkSize.y, chunkSize.z / 2);
+            GameObject ChunkObj = Instantiate(chunkPrefab, pos, Quaternion.identity);
+            Chunk chunkScript = ChunkObj.GetComponent<Chunk>();
+            if (chunkScript != null)
             {
-                new Vector3(0, 0, 0), // 0
-                new Vector3(0, 1, 0), // 1
-                new Vector3(1, 1, 0), // 2
-                new Vector3(1, 0, 0), // 3
-    
-                new Vector3(1, 0, 0), // 4
-                new Vector3(1, 1, 0), // 5 
-                new Vector3(1, 1, 1), // 6
-                new Vector3(1, 0, 1), // 7 
-    
-                new Vector3(1, 0, 1), // 8
-                new Vector3(1, 1, 1), // 9
-                new Vector3(0, 1, 1), // 10
-                new Vector3(0, 0, 1), // 11
-    
-                new Vector3(0, 0, 1), // 12
-                new Vector3(0, 1, 1), // 13
-                new Vector3(0, 1, 0), // 14  <-- The stubborn stray!
-                new Vector3(0, 0, 0), // 15
-    
-                new Vector3(0, 1, 0), // 16
-                new Vector3(0, 1, 1), // 17
-                new Vector3(1, 1, 1), // 18
-                new Vector3(1, 1, 0), // 19
-    
-                new Vector3(0, 0, 0), // 20
-                new Vector3(1, 0, 0), // 21
-                new Vector3(1, 0, 1), // 22
-                new Vector3(0, 0, 1)  // 23
-            };
-
-            triangles = new int[]
-            {
-                // Front Face
-                0, 1, 2,
-                0, 2, 3,
-    
-                // Right Face
-                4, 5, 6,
-                4, 6, 7,
-    
-                // Back Face
-                8, 9, 10,
-                8, 10, 11,
-    
-                // Left Face
-                12, 13, 14,
-                12, 14, 15,
-    
-                // Top Face
-                16, 17, 18,
-                16, 18, 19,
-    
-                // Bottom Face
-                20, 21, 22,
-                20, 22, 23
-            };
-        }
-
-        private void UpdateMesh()
-        {
-            if (mesh == null) return;
-
-            mesh.Clear(); ;
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-
-            mesh.RecalculateNormals();
+                chunkScript.SetChunk(chunkSize);
+            }
         }
 
         void Update()
